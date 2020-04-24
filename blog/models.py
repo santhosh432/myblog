72306d88c7e41a6d5865935d7ed18a8d39501815
santhosh <santhoshcusat17@gmail.com>
2020-04-24 21:31:51 +0530

    post model created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    posted_date = models.DateTimeField(default=timezone.now())
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{0}'.format(self.title)
